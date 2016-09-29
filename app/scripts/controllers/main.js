9543e1ad90d22dd420a5f98318817424b03a246c
Rishabh Saxena <orishabh1@gmail.com>
2016-09-29 15:52:05 +0530

    change to angular1

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webSummitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webSummitApp
+ */
+angular.module('webSummitApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
