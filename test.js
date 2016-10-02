00838aecf8a7e7f95e2d82f200a225c92aa1b31f
Rishabh Saxena <orishabh1@gmail.com>
2016-10-02 20:34:53 +0530

    more descriptive ble connection file

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 console.log("ble ecg interfacing");
 
+// CONSTANTS
 var noble = require('noble');
 const fs = require('fs');
 var ARRAY_SIZE = 1000;
@@ -14,27 +15,27 @@ var ws;
 var ECG_DATA_PORT = 8097;
 var connected = false;
 
+// start scanning after connection is established
 noble.on('stateChange', function(state) {
     console.log("statechange:" + state);
     if (state === 'poweredOn') {
         console.log("Bluetooth is on here");
         noble.startScanning();
-    } 
-    else {
+    } else {
         console.log("Bluetooth not on");
         noble.stopScanning();
     }
 });
 
+// if the connected device is recognised, call function
 noble.on('discover', function(peripheral) {
-
     var advertisement = peripheral.advertisement;
     var localName = advertisement.localName;
     console.log(localName);
+    // OSX will have `undefined` as peripheral name if not connected earlier
     if (localName == undefined || localName.substring(0, 6) != "ADS129") {
         return;
-    } 
-    else {
+    } else {
         noble.stopScanning();
     }
     explore(peripheral);
@@ -48,7 +49,7 @@ function explore(peripheral) {
     peripheral.connect(function(error) {
         console.log('connected to peripheral: ' + peripheral.uuid);
         peripheral.writeHandle('0x0013', new Buffer([0x01, 0x00]), 0, function(error) {
-            console.log("inside loop");
+            console.log("handle written");
         });
 
         peripheral.discoverServices(['2d0d'], function(error, services) {
@@ -58,13 +59,11 @@ function explore(peripheral) {
                 //console.log(characteristics);
                 var characteristic = characteristics[0];
                 characteristic.on('read', function(data, isNotification) {
-                    //console.log( data[0]*65536+data[1]*256+data[2] ,data[3]*65536+data[4]*256+data[5] );
-
+                    // 8 bits for each data entry X 3
                     x = data[0] * 65536 + data[1] * 256 + data[2];
                     y = data[3] * 65536 + data[4] * 256 + data[5];
                 });
             });
-
         });
     });
 }
