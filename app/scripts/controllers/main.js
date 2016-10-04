ff98dc4cd28cbdd8aebd8db4e5cfadf4d90d725b
Rishabh Saxena <orishabh1@gmail.com>
2016-10-05 00:02:33 +0530

    move script to controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,20 @@
  */
 angular.module('webSummitApp')
   .controller('MainCtrl', function () {
-    
+	// Plot.ly
+
+	var trace1 = {
+	  x: [1, 2, 3, 4], 
+	  y: [10, 15, 13, 17],
+	  name: 'heartrate',
+	  type: 'scatter'
+	};
+	var trace2 = {
+	  x: [1, 2, 3, 4], 
+	  y: [16, 5, 11, 9],
+	  name: 'oxy-something', 
+	  type: 'scatter'
+	};
+	var data = [trace1, trace2];
+	Plotly.newPlot('plot', data);
   });
