a6f0fd26be350d37e2a3dde2f951ca8f9b25b9fd
Rishabh Saxena <orishabh1@gmail.com>
2016-10-06 11:55:59 +0530

    modify queueing model logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,8 +9,13 @@
  */
 angular.module('webSummitApp')
   .controller('MainCtrl', ['$scope', 'mySocket', function ($scope, mySocket) {
-    $scope.points_x = [1, 2, 3, 4];
-    $scope.points_y = [10, 15, 13, 17];
+    $scope.points_x = new Array(100);
+    $scope.points_y = new Array(100).fill(5);
+
+    // fix values of x, as in a live graph only the value of y will be enqueued
+    for (var i=0; i<100; i++) {
+      $scope.points_x[i] = i;
+    }
 
     var trace1 = {
       x: $scope.points_x,
@@ -24,8 +29,8 @@ angular.module('webSummitApp')
 
     mySocket.on('pointData', function(point) {
       $scope.$apply(function() {
-        $scope.points_x.shift();
-        $scope.points_x.push(point.value_x);
+        // $scope.points_x.shift();
+        // $scope.points_x.push(point.value_x);
         $scope.points_y.shift();
         $scope.points_y.push(point.value_y);
 
