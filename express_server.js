c22caffc10528b98e74de2573e483ade62d1daf9
Rishabh Saxena <orishabh1@gmail.com>
2016-10-09 14:57:42 +0530

    allow requests with credentials

--- express_server.js
+++ express_server.js
@@ -14,9 +14,9 @@ app.use(express.static(__dirname + '/public'));
 
 // MIDDLEWARE: set cross origin sharing headers
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:9000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
